4abf8e52e6c7d7c277a1475b6fd4456e7a41a4ab
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-22 17:23:34 -0400

    Update server.js

--- to-the-moon-backend/server.js
+++ to-the-moon-backend/server.js
@@ -1,6 +1,10 @@
 const app = require('express')();
 const PORT = 8080;
 
+const userRoute = require("./routes/userRoute");
+
+app.use("/api/users", userRoute);
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
