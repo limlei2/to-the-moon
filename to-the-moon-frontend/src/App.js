445edf5001c2236250afc00ceba7e6aa6dadd558
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-22 16:28:44 -0400

    cleanup

--- to-the-moon-frontend/src/App.js
+++ to-the-moon-frontend/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage"
+
+import Navbar from "./components/Navbar"
+import LoggedInNavbar from "./components/LoggedInNavbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+            <Route index element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/register" element={<Register />}/>
+            <Route path="/login" element={<Login />}/>
+            <Route path="*" element={<NoPage />}/>
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
