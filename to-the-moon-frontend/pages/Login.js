305c1d5c0d54b49f81dba8857fa92bd99a48e224
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-03-22 16:14:27 -0400

    files

---
+++ to-the-moon-frontend/pages/Login.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+
+    async function submit(e){
+        e.preventDefault();
+        
+    }
+
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default Login
