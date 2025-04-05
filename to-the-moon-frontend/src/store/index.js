6243ad44c486ee425a7124badb31af1ce9b8e047
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-04-05 02:39:23 -0400

    persist redux

--- to-the-moon-frontend/src/store/index.js
+++ to-the-moon-frontend/src/store/index.js
@@ -1,10 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice';
 
-const store = configureStore({
-    reducer:{
-        user: userReducer
-    }
-})
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {
+    user: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
