1e3155438838b23e5922d833e724cc6a8164a199
limlei2 <87996616+limlei2@users.noreply.github.com>
2025-04-24 17:43:55 -0400

    Update Navbar.js

--- to-the-moon-frontend/src/components/Navbars/Navbar.js
+++ to-the-moon-frontend/src/components/Navbars/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
             <img src="https://cdn0.iconfinder.com/data/icons/coloricons/50/50X50-02-1024.png" className="h-14" alt="to-the-moon"/>
             <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white" style={{fontFamily: 'TypeMachine, sans-serif'}}>to-the-moon</span>
         </a>
+        
         <div className="flex">
           <Link to="/login"><button type="button" className="border border-white text-white bg-black hover:bg-white hover:text-black transition focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-3xl text-md px-6 py-2 text-center mx-2">Login</button></Link>
           <Link to="/register"><button type="button" className="text-white bg-indigo-600 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-md px-6 py-2 text-center mx-2">Get Started</button></Link>
